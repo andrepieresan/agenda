962c23ca253bfe728f173c6dcf2d9f57a24aaeef
root <andrepieresan@gmail.com>
2021-11-09 15:24:05 -0500

    salve a terra

--- routes.js
+++ routes.js
@@ -1,11 +1,13 @@
 const express= require('express');
 const route = express.Router(); 
 
-
+// rotas da homepage
 
 route,get('/', (req, res)=>{
-    res.send('')
+    res.send(`<form> method=`)
 })
-
-
+route.post('/',()=>{
+    res.send('<h1> nova rota de post (home) </h1>')
+})
+fafafafa
 modeul.exports = route;
