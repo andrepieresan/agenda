671a68c81d5fb7a3d560e2ac2f754613da2ea62e
root <andrepieresan@gmail.com>
2021-11-09 20:05:30 -0500

    v1.1

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,23 @@
 const express= require('express')
 const route = express.Router(); 
 const homeController = require('./controllers/homeController')
+const loginController = require('./controllers/loginController')
+const cadastroController = require('./controllers/cadastroController')
+
+
+
 // rotas da homepage (LOGIN)
 
 route.get('/', homeController.homepage);
 
+// rotas de cadastro
+route.get('', cadastroController.index ); 
 
-route.post('/',(req,res)=>{
-    res.send('<h1> nova rota de post (home) </h1>')
-});
 
-module.exports = route;
 
 // rotas de agenda (CRUD)
+
+
+
+
+module.exports = route;
